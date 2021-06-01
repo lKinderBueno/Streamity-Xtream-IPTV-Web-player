ea9dcfeda3138f46bbee20e99000b8f47be1a8f7
Elio Milite <e.milite4@studenti.unisa.it>
2021-06-01 09:50:06 +0200

    config.js

--- public/config.js
+++ public/config.js
@@ -1,19 +1,14 @@
-window.isDebug = 1;
-
 /*----- Player name -----*/
 window.playername = "IPTVEditor Web Player";
 
-
 /*----- DNS -----*/
 //Iptv provider dns url (for example "http://domain.com:80")
-if(window.isDebug){
-  window.dns =  "http://localhost:3000/webplayer";
-}else window.dns = "https://api.iptveditor.com/webplayer";
+window.dns = "http://domain.com:80";
 
 /*----- CORS -----*/
 /*/ Change on false if iptv provider has the Access-Control-Allow-Origin set on "*" or allows your player domain.
 Leave on "true" in the other case. Player will use "proxy.php" file to manage the requests.  */
-window.cors = false; //false
+window.cors = true; //false
 
 /*---- HTTPS -----*/
 /* If streams are using an ssl protol change this box on true.*/
@@ -22,6 +17,4 @@ window.https = false;  //true;
 /*----- TMDB API [OPTIONAL] -----*/
 /* By default player will use movie info from the provider. In case these info are missing it will be used tmdb as alternative
 Here you can get a tmdb api key: https://developers.themoviedb.org/3/getting-started/introduction  */
-window.tmdb = "a2764023c82b647eac48485b4deac0bf";
-
-window.m3u8warning = true;
+window.tmdb = "";
